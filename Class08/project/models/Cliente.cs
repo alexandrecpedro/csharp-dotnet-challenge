456252bef974ef6382b05682e8d3ed50ac12287f
Alexandre Pedro <alexandrecpedro@gmail.com>
2022-12-05 07:03:42 -0300

    Class 08 - Logical Reasoning: Dynamic Objects | Struct

---
+++ Class08/project/models/Cliente.cs
@@ -0,0 +1,9 @@
+namespace Logica.models;
+
+struct Cliente
+{
+    public string ID { get;set; }
+    public string Nome { get;set; }
+    public string Telefone { get;set; }
+    public string Email { get;set; }
+}
---
+++ Class08/project/models/ContaCorrente.cs
@@ -0,0 +1,8 @@
+namespace Logica.models;
+
+struct ContaCorrente
+{
+    public string IdCliente { get; set; }
+    public double Valor { get; set; }
+    public DateTime Data { get; set; }
+}
